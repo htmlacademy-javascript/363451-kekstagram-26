e85e6b72fe42373bbe9c487e3ae2c47e2dc72c50
Keks <keks@htmlacademy.ru>
2022-07-21 18:19:26 +0300

    :heavy_check_mark: Сборка #12

---
+++ 12/js/form.js
@@ -0,0 +1,81 @@
+import { isEscapeKey } from './util.js';
+import { pristine, onSubmitForm } from './form-validation.js';
+import { setupZoom, destroyZoom } from './zoom.js';
+import { setupEffects, destroyEffects } from './effect.js';
+
+const FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
+const uploadFile = document.querySelector('#upload-file');
+const uploadOverlay = document.querySelector('.img-upload__overlay');
+const bodyContainer = document.querySelector('body');
+const uploadClose = document.querySelector('.img-upload__cancel');
+const imgUploadForm = document.querySelector('.img-upload__form');
+const imgUploadPreview = document.querySelector('.img-upload__preview img');
+const textHashtags = document.querySelector('.text__hashtags');
+const textDescription = document.querySelector('.text__description');
+
+uploadFile.addEventListener('change', () => {
+  const file = uploadFile.files[0];
+  const fileName = file.name.toLowerCase();
+
+  const matches = FILE_TYPES.some((it) => fileName.endsWith(it));
+
+  if (matches) {
+    imgUploadPreview.src = URL.createObjectURL(file);
+  }
+});
+
+const stopPropagationEsc = (evt) => {
+  if (isEscapeKey(evt)) {
+    evt.stopPropagation();
+  }
+};
+
+const closeUploadForm = () => {
+  uploadOverlay.classList.add('hidden');
+  bodyContainer.classList.remove('modal-open');
+  imgUploadForm.reset();
+  imgUploadPreview.style = 'transform: scale(1)';
+  imgUploadPreview.style.filter = 'none';
+  imgUploadPreview.className = 'none';
+  pristine.reset();
+  removeEvent();
+  destroyZoom();
+  destroyEffects();
+};
+
+const onEscKeydown = (evt) => {
+  const modalError = bodyContainer.querySelector('.error');
+  if(isEscapeKey(evt) && !modalError) {
+    evt.preventDefault();
+    closeUploadForm();
+  }
+};
+
+const onClosedForm = (evt) => {
+  evt.preventDefault();
+  closeUploadForm();
+};
+
+function removeEvent () {
+  uploadClose.removeEventListener('click', onClosedForm);
+  document.removeEventListener('keydown', onEscKeydown);
+  textHashtags.removeEventListener('keydown', stopPropagationEsc);
+  textDescription.removeEventListener('keydown', stopPropagationEsc);
+  imgUploadForm.removeEventListener('submit', onSubmitForm);
+}
+
+const openUploadForm = () => {
+  uploadOverlay.classList.remove('hidden');
+  bodyContainer.classList.add('modal-open');
+  uploadClose.addEventListener('click', onClosedForm);
+  document.addEventListener('keydown', onEscKeydown);
+  textHashtags.addEventListener('keydown', stopPropagationEsc);
+  textDescription.addEventListener('keydown', stopPropagationEsc);
+  imgUploadForm.addEventListener('submit', onSubmitForm);
+  setupZoom();
+  setupEffects();
+};
+
+uploadFile.addEventListener('change', openUploadForm);
+
+export {openUploadForm, closeUploadForm, bodyContainer};
---
+++ 12/js/picture.js
@@ -0,0 +1,28 @@
+import { openBigPicture } from './big-picture.js';
+
+const pictureTemplate = document.querySelector('#picture')
+  .content
+  .querySelector('.picture');
+const pictureList = document.querySelector('.pictures');
+const filtersElement = document.querySelector('.img-filters');
+const fragment = document.createDocumentFragment();
+
+const renderPhotos = (photos) => {
+  photos.forEach((photo) => {
+    const pictureElement = pictureTemplate.cloneNode(true);
+    pictureElement.querySelector('.picture__img').src = photo.url;
+    pictureElement.querySelector('.picture__comments').textContent = photo.comments.length;
+    pictureElement.querySelector('.picture__likes').textContent = photo.likes;
+    fragment.append(pictureElement);
+
+    pictureElement.addEventListener('click', () => {
+      openBigPicture(photo);
+    });
+  });
+  pictureList.querySelectorAll('.picture').forEach((item) => item.remove());
+  pictureList.append(fragment);
+
+  filtersElement.classList.remove('img-filters--inactive');
+};
+
+export {renderPhotos};
